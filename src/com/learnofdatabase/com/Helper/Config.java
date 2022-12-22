6d9d0e0e23951e43e56046becef060e44f87a26b
Michał Francuz <104276165+michal9696@users.noreply.github.com>
2022-12-22 23:29:59 +0100

    Learning of connecting to database - writing to our Database. Problem with putting date

---
+++ src/com/learnofdatabase/com/Helper/Config.java
@@ -0,0 +1,9 @@
+package com.learnofdatabase.com.Helper;
+
+public class Config {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "@M8!f1zki96";
+    protected String dbName = "loginscheme";
+}
--- src/com/learnofdatabase/com/Main.java
+++ src/com/learnofdatabase/com/Main.java
@@ -1,16 +1,31 @@
 package com.learnofdatabase.com;
 
+import com.learnofdatabase.com.Helper.DBHandler;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Main {
+    static private DBHandler dbHandler;
+    static private Connection connection;
+    static private PreparedStatement preparedStatement;
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        //Class.forName("com.mysql.cj.jdbc.Driver");
-        // calling this class is for older version of java
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/loginscheme",
-                "root", "@M8!f1zki96");
 
-        System.out.println("Connected to database " + connection.getCatalog());
+        dbHandler = new DBHandler();
+        connection = dbHandler.getDbConnection();
+
+        String insert = "INSERT INTO usersofapp(firstname,lastname,username,address,yearofborn)"
+                + "VALUES(?,?,?,?,?)";
+
+        preparedStatement = connection.prepareStatement(insert);
+
+        preparedStatement.setString(1, "Michael");
+        preparedStatement.setString(2, "French");
+        preparedStatement.setString(3, "michaelf");
+        preparedStatement.setString(4, "64276 Ewa Beach");
+        preparedStatement.setInt(5, 1996);
+
     }
 }
