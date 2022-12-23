360295bac6e10ed736b70f6246430994970a3052
Michał Francuz <104276165+michal9696@users.noreply.github.com>
2022-12-23 22:35:49 +0100

    Learning of connecting to database - reading from our Database.

--- src/com/learnofdatabase/com/Main.java
+++ src/com/learnofdatabase/com/Main.java
@@ -2,10 +2,7 @@ package com.learnofdatabase.com;
 
 import com.learnofdatabase.com.Helper.DBHandler;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     static private DBHandler dbHandler;
@@ -17,18 +14,42 @@ public class Main {
         dbHandler = new DBHandler();
         connection = dbHandler.getDbConnection();
 
-        String insert = "INSERT INTO usersofapp(firstname,lastname,username,address,yearofborn)"
-                + "VALUES(?,?,?,?,?)";
+        //writeToDataBase();
+        readFromDataBase();
+
+    }
+
+    public static void writeToDataBase() throws SQLException {
+
+        String insert = "INSERT INTO usersofapp(ID,firstname,lastname,username,address,yearofborn)"
+                + "VALUES(?,?,?,?,?,?)";
 
         preparedStatement = connection.prepareStatement(insert);
 
-        //preparedStatement.setInt(1, 5);
-        preparedStatement.setString(1, "Adalbert");
-        preparedStatement.setString(2, "Brave");
-        preparedStatement.setString(3, "adalbertb");
-        preparedStatement.setString(4, "02941 Palma de Mallorca");
-        preparedStatement.setInt(5, 1992);
+        preparedStatement.setInt(1, 7);
+        preparedStatement.setString(2, "Sawyer");
+        preparedStatement.setString(3, "James");
+        preparedStatement.setString(4, "sawyerj");
+        preparedStatement.setString(5, "40165 West Coast");
+        preparedStatement.setInt(6, 1987);
         preparedStatement.executeUpdate();
+
+    }
+
+    public static void readFromDataBase() throws SQLException {
+
+        String query = "SELECT * from usersofapp";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            System.out.println("Names: " + resultSet.getString("firstname") + " "
+                    + resultSet.getString("lastname") + "     "
+                    + " Username: " + resultSet.getString("username") + "     "
+                    + " Home address: " + resultSet.getString("address") + "     "
+                    + " Year of born: " + resultSet.getInt("yearofborn"));
+        }
 
     }
 }
